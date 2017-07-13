14c0a7806a8c9f9f85342b7aea64daeb579c51ef
M-Nishiura <ma_sa_ta_ka_02@yahoo.co.jp>
2017-07-13 23:50:43 +0900

    仮作成した変数

---
+++ src/Variable/Player.java
@@ -0,0 +1,15 @@
+package Variable;
+
+public class Player {
+  String code;
+  String name;
+  int piece;
+  int cash;
+
+  Player(String code, String name, int cash) {
+    this.code = code;
+    this.name = name;
+    this.cash = cash;
+  }
+
+}
